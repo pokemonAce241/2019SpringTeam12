1372a533cfd2a7608ca813843f32ceb0af45634d
Tam Le <thle@ncsu.edu>
2018-10-26 12:26:03 -0400

    Refactored filter components to be named toolbar

--- pollinator-garden/src/app/garden/garden.module.ts
+++ pollinator-garden/src/app/garden/garden.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CanvasComponent } from './canvas/canvas.component';
 import { PlantListComponent } from './plant-list/plant-list.component';
 import { MyGardensComponent } from './my-gardens/my-gardens.component';
-import { FilterComponent } from './filter/filter.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MainGardenComponent } from './main-garden/main-garden.component';
 import { RouterModule } from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -15,7 +15,7 @@ import { SearchComponent } from './search/search.component';
     RouterModule,
     NgbModule
   ],
-  declarations: [CanvasComponent, PlantListComponent, MyGardensComponent, FilterComponent, MainGardenComponent, SearchComponent],
+  declarations: [CanvasComponent, PlantListComponent, MyGardensComponent, ToolbarComponent, MainGardenComponent, SearchComponent],
   bootstrap: [MainGardenComponent]
 })
 export class GardenModule { }
--- pollinator-garden/src/app/garden/toolbar/toolbar.component.spec.ts
+++ pollinator-garden/src/app/garden/toolbar/toolbar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FilterComponent } from './filter.component';
+import { ToolbarComponent } from './toolbar.component';
 
-describe('FilterComponent', () => {
-  let component: FilterComponent;
-  let fixture: ComponentFixture<FilterComponent>;
+describe('ToolbarComponent', () => {
+  let component: ToolbarComponent;
+  let fixture: ComponentFixture<ToolbarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FilterComponent ]
+      declarations: [ ToolbarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FilterComponent);
+    fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
