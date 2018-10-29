ef6176204bd57a86641ae17450031d095e55e055
Tam Le <thle@ncsu.edu>
2018-10-29 11:01:49 -0400

    Converted plant list into a canvas element, added to plant-list.ts file

--- pollinator-garden/src/app/garden/plant-list/plant-list.component.ts
+++ pollinator-garden/src/app/garden/plant-list/plant-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { InstanceService } from 'src/app/services/instance.service';
 
 @Component({
   selector: 'app-plant-list',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlantListComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('canvasEl') canvasEl: ElementRef;
+  
+  /** Canvas 2d context */
+  private context: CanvasRenderingContext2D;
+
+  // plants: Plant[];
+
+  constructor(
+    // private router: Router,
+    // private instanceService: InstanceService
+  ) { }
+
+  ngAfterViewInit() { 
+    let canvas = document.getElementById('plant-list-canvas') as HTMLCanvasElement;
+    let parent = document.getElementById('canvasContainer') as HTMLDivElement;
+    canvas.height = parent.offsetHeight * .95;
+    canvas.width = parent.offsetWidth * .95;
+    this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
+
+  }
 
   ngOnInit() {
+    var img = new Image();
+    img.src = "https://www.fiftyflowers.com/site_files/FiftyFlowers/Image/Product/peony-sarah-bernhardt-bloom-500_b572106d.jpg";
+
+    img.onload = () => {
+      this.context.drawImage(img, 10, 10, 100, 100);
+    }
+    // this.getPlants();
   }
+
+  // private draw() {
+  //     this.plants.forEach(instance => {
+  //       var img = new Image();
+  //       img.src = instance.front_image_path;
+
+  //       img.onload = () => {
+  //         this.context.drawImage(img, instance.x, instance.y, 100, 100);
+  //       }
+  //     })
+  // }
+
+  // getPlants() {
+  //   this.instanceService.getPlants()
+  //     .subscribe(res => {
+  //       console.log(res);
+  //       this.plants = res;
+  //       this.draw();
+  //     })
+  // }
 
 }
