bdf2cc37de69da92d5da75989c0cdef156432515
cndrotor <cndrotor@ncsu.edu>
2018-11-02 11:35:52 -0400

    added basic click and moving and image around on the canvas.

--- pollinator-garden/src/app/garden/plant-list/plant-list.component.ts
+++ pollinator-garden/src/app/garden/plant-list/plant-list.component.ts
@@ -14,51 +14,67 @@ export class PlantListComponent implements OnInit {
   
   /** Canvas 2d context */
   private context: CanvasRenderingContext2D;
-
-  // plants: Plant[];
+  private imgDim: any;
+  private toggle: boolean;
+  private img: HTMLImageElement;
+  private active: boolean;
 
   constructor(
-    // private router: Router,
-    // private instanceService: InstanceService
-  ) { }
+  ) {
+    this.imgDim = {};
+    this.img = new Image();
+    this.toggle = false;
+    this.active = true;
+  }
 
   ngAfterViewInit() { 
     let canvas = document.getElementById('plant-list-canvas') as HTMLCanvasElement;
-    let parent = document.getElementById('canvasContainer') as HTMLDivElement;
-    canvas.height = parent.offsetHeight * .95;
-    canvas.width = parent.offsetWidth * .95;
+    canvas.height = 500;
     this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
+    document.addEventListener('click', (ev) => {
+      if((ev.x > this.imgDim.x && ev.x < this.imgDim.x + this.imgDim.width) &&
+          (ev.y - 200 > this.imgDim.y && ev.y - 200 < this.imgDim.y + this.imgDim.width) &&
+          !this.toggle) {
+            this.toggle = !this.toggle;
+            console.log(this.toggle);
+            return;
+          }
 
+      if(this.toggle) {
+        if(ev.x < canvas.width && ev.x > 0 && ev.y - 200 > 0 && ev.y - 200 < canvas.height) {
+        this.imgDim.x = ev.x - this.imgDim.width * .5;
+        this.imgDim.y = ev.y - 200 - this.imgDim.height * .5;
+        this.context.clearRect(0,0,canvas.width, canvas.height);
+        this.context.drawImage(this.img, this.imgDim.x, this.imgDim.y, 100, 100);
+        this.toggle = !this.toggle;
+        console.log(this.toggle);
+        } else {
+          this.context.clearRect(0,0,canvas.width, canvas.height);
+          alert("Invalid location.\nPlease try again.");
+          this.context.drawImage(this.img, 10, 10, 100, 100);
+          this.toggle = !this.toggle;
+          console.log(this.toggle);
+        }
+      }
+    });
+
+    canvas.onmousemove = (ev) => {
+      if(this.toggle) {
+        this.context.clearRect(0,0,canvas.width, canvas.height);
+        this.context.drawImage(this.img, ev.x - this.imgDim.width * .5, ev.y - 200 - this.imgDim.height * .5, 100, 100);
+      }
+    }
   }
 
   ngOnInit() {
-    var img = new Image();
-    img.src = "https://www.fiftyflowers.com/site_files/FiftyFlowers/Image/Product/peony-sarah-bernhardt-bloom-500_b572106d.jpg";
-
-    img.onload = () => {
-      this.context.drawImage(img, 10, 10, 100, 100);
+    this.img.src = "https://www.fiftyflowers.com/site_files/FiftyFlowers/Image/Product/peony-sarah-bernhardt-bloom-500_b572106d.jpg";
+    this.img.onload = () => {
+      this.context.drawImage(this.img, 10, 10, 100, 100);
+      this.imgDim.x = 10;
+      this.imgDim.y = 10;
+      this.imgDim.width = 100;
+      this.imgDim.height = 100;
+      // console.log(this.imgDim);
     }
-    // this.getPlants();
   }
-
-  // private draw() {
-  //     this.plants.forEach(instance => {
-  //       var img = new Image();
-  //       img.src = instance.front_image_path;
-
-  //       img.onload = () => {
-  //         this.context.drawImage(img, instance.x, instance.y, 100, 100);
-  //       }
-  //     })
-  // }
-
-  // getPlants() {
-  //   this.instanceService.getPlants()
-  //     .subscribe(res => {
-  //       console.log(res);
-  //       this.plants = res;
-  //       this.draw();
-  //     })
-  // }
-
 }
