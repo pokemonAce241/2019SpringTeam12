fd374f051e655363789a6345ede38b422753b82a
Kevin Bartushak <kabartus@ncsu.edu>
2018-11-25 16:05:41 -0500

    Finished karma testing for my-garden branch

--- pollinator-garden/src/app/services/garden.service.spec.ts
+++ pollinator-garden/src/app/services/garden.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
-import { GardenService } from './garden.service';
+import { GardenService, Garden } from './garden.service';
 import { HttpClient } from '@angular/common/http';
 
 describe('GardenService', () => {
 
-  const httpSpy = jasmine.createSpyObj('HttpClient', ['get']);
+  const httpSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+  httpSpy.post.and.returnValue(of({}));
+
+  let service: GardenService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,10 +17,32 @@ describe('GardenService', () => {
         { provide: HttpClient, useValue: httpSpy }
       ]
     })
+    
   });
 
   it('should be created', () => {
-    const service: GardenService = new GardenService(httpSpy);
+    service = new GardenService(httpSpy);
     expect(service).toBeTruthy();
+  });
+
+  it('should return a list of gardens', () => {
+    service = new GardenService(httpSpy);
+    
+    service.getGardens();
+    expect(httpSpy.get.calls.any()).toBe(true, 'get called');
+  });
+
+  it('should return a list of gardens for a user', () => {
+    service = new GardenService(httpSpy);
+
+    service.getGardensForUser(1);
+    expect(httpSpy.get.calls.any()).toBe(true, 'get called');
+  });
+
+  it('should create a new garden', () => {
+    service = new GardenService(httpSpy);
+
+    service.createGarden(new Garden());
+    expect(httpSpy.post.calls.any()).toBe(true, 'post called');
   });
 });
