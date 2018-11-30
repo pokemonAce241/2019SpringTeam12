f5a124090c9cde861d3ea92cada50c4f01f1a323
Kevin Bartushak <kabartus@ncsu.edu>
2018-11-30 12:16:45 -0500

    Create filter logic, need to apply to all fields

--- pollinator-garden/src/app/garden/plant-list/plant-list.component.ts
+++ pollinator-garden/src/app/garden/plant-list/plant-list.component.ts
@@ -11,7 +11,46 @@ export class PlantListComponent implements OnInit {
   plants: Plant[];
   filteredPlants: Plant[];
 
+  seasonFilters = {
+    "summer": false,
+    "spring": false,
+    "fall": false,
+    "winter": false
+  }
 
+  regionFilters = {
+    "mountain": false,
+    "piedmont": false,
+    "coast": false
+  }
+
+  colorFilters = {
+    "red": false,
+    "blue": false,
+    "purple": false,
+    "pink": false,
+    "yellow": false,
+    "white": false,
+    "orange": false,
+    "green": false,
+    "other": false
+  }
+
+  typeFilters = {
+    "vine": false,
+    "shrub": false,
+    "perennial": false,
+    "annual": false
+  }
+
+  soilFilters = {
+    "wet": false,
+    "moist": false,
+    "dry": false
+  }
+
+  minHeight: number;
+  maxHeight: number;
 
   constructor(
     private plantService: PlantService
@@ -30,7 +69,46 @@ export class PlantListComponent implements OnInit {
   }
 
   filterPlants() {
+    this.filteredPlants = this.plants;
 
+    this.filteredPlants = this.filteredPlants.filter(plant => {
+      var match = true;
+
+      // Season
+      if (this.seasonFilters.spring && !plant.seasons.includes("spring")) {
+        match = false;
+      }
+      if (this.seasonFilters.summer && !plant.seasons.includes("summer")) {
+        match = false;
+      }
+      if (this.seasonFilters.fall && !plant.seasons.includes("fall")) {
+        match = false;
+      }
+      if (this.seasonFilters.winter && !plant.seasons.includes("winter")) {
+        match = false;
+      }
+
+      return match;
+    });
+
+    this.filteredPlants = this.filteredPlants.filter(plant => {
+      var match = true;
+
+      // Region
+      if (this.regionFilters.mountain && !plant.regions.includes("mountain")) {
+        match = false;
+      }
+      if (this.regionFilters.piedmont && !plant.regions.includes("piedmont")) {
+        match = false;
+      }
+      if (this.regionFilters.coast && !plant.regions.includes("coast")) {
+        match = false;
+      }
+
+      return match;
+    });
+
+    console.log(this.filteredPlants);
   }
 
 }
