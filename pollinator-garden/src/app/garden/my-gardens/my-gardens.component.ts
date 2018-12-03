a502e15ed1fabe1adecec75b33463c091eddeebf
Tam Le <thle@ncsu.edu>
2018-12-03 10:22:23 -0500

    Implemented deleteGarden

--- pollinator-garden/src/app/garden/my-gardens/my-gardens.component.ts
+++ pollinator-garden/src/app/garden/my-gardens/my-gardens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import {Garden, GardenService } from 'src/app/services/garden.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -7,16 +7,21 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-my-gardens',
   templateUrl: './my-gardens.component.html',
   styleUrls: ['./my-gardens.component.css'],
+  providers: [GardenService, NgbModal]
 })
 export class MyGardensComponent implements OnInit {
+  @ViewChild('nameGardenModal') nameGardenModal: ElementRef;
 
   gardens: Garden[]
 
   // Name of garden entered by user
-  textValue: string
+  textValue: string = ""
 
   // For now
   user_id = 1;
+
+  lengthError: boolean = false
+  uniqueNameError: boolean = false
 
   constructor(
     private router: Router,
@@ -38,8 +43,8 @@ export class MyGardensComponent implements OnInit {
 
   public goToGarden(id: number) {
     this.router.navigate(['/garden', id]);
+    this.nameGardenModal.nativeElement.classList.remove("fade")
     this.modal.dismissAll();
-    
   }
 
   public createNewGarden() {
@@ -47,12 +52,42 @@ export class MyGardensComponent implements OnInit {
     garden.name = this.textValue;
     garden.user_id = this.user_id;
 
-    // Need to check if name given is unique & if between 1 & 100 characters
-    // error will be caught below
+    // Need to check for errors
+    if (this.textValue.length > 50 || this.textValue.length < 1) {
+      this.lengthError = true
+    } else {
+      this.lengthError = false
+    }
+
+    if (this.gardens.find(val => val.name === this.textValue) != undefined) {
+      this.uniqueNameError = true
+    } else {
+      this.uniqueNameError = false
+    }
+
+    if (this.lengthError || this.uniqueNameError) {
+      return
+    }
+
     this.gardenService.createGarden(garden)
     .subscribe(res => {
       this.goToGarden(res.id)
     })
   }
 
+  public deleteGarden(id: number) {
+    if (confirm("Are you sure you want to delete this garden?").valueOf()) {
+      this.gardenService.deleteGarden(id)
+      .subscribe(res => {
+        alert("Garden has been deleted.")
+        console.log("Garden " + id + "deleted.")
+      })
+    }
+  }
+  
+  public cancelNameGardenModal() {
+    this.lengthError = false;
+    this.uniqueNameError = false;
+    console.log(this.lengthError, this.uniqueNameError);
+  }
 }
